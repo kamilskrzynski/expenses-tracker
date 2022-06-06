b3674608c981c7855ff8eef1048ba3ceaae85804
KamilSkrzynski <kamilskrzynski95@gmail.com>
2022-06-06 10:21:15 +0200

    Initial commit

---
+++ ExpensesTracker/ExpensesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerApp.swift
+//  ExpensesTracker
+//
+//  Created by Kamil Skrzyński on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
